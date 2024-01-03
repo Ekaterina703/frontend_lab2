a21d7b9ad04fa835d524b71c4961dd5cb243d296
Ekaterina703 <Ekaterina703>
2024-01-03 14:29:41 +0700

    EDIT moved swiper code to script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
   
     // If we need pagination
@@ -18,4 +18,9 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
-  });
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  })
